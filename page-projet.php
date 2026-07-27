e253733c55fe1738c82f49453bce9df2e916771b
Amine <benaminmedfr@gmail.com>
2026-07-27 17:40:46 +0100

    feat(projet): enable Lightbox zoom popup modal on partner orbit nodes and central Globuild emblem

--- page-projet.php
+++ page-projet.php
@@ -307,11 +307,13 @@ get_header();
             <div class="globuild-orbit-ring outer-ring"></div>
 
             <!-- Central Globuild Sphere Core Badge -->
-            <div class="globuild-orbit-center">
-                <div class="center-pulse-glow"></div>
-                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/favicon-circle.png" alt="Globuild Logo Core" class="center-brand-logo">
-                <span class="center-brand-tag">GLOBUILD</span>
-            </div>
+            <a href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/favicon-circle.png" data-lightbox="partners-gallery" data-title="GLOBUILD" class="globuild-orbit-center-link">
+                <div class="globuild-orbit-center">
+                    <div class="center-pulse-glow"></div>
+                    <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/favicon-circle.png" alt="Globuild Logo Core" class="center-brand-logo">
+                    <span class="center-brand-tag">GLOBUILD</span>
+                </div>
+            </a>
 
             <!-- 14 Radial Partner Nodes arranged in Globuild Sphere shape -->
             <div class="globuild-orbit-nodes">
@@ -319,16 +321,18 @@ get_header();
                     $img_url = get_template_directory_uri() . '/assets/img/' . $partner['logo'];
                 ?>
                     <div class="orbit-partner-node" style="--index: <?php echo $index; ?>; --total: <?php echo $total; ?>;">
-                        <div class="orbit-partner-node-inner">
-                            <span class="node-number"><?php echo sprintf('%02d', $partner['num']); ?></span>
-                            <div class="node-logo-wrapper">
-                                <img src="<?php echo esc_url($img_url); ?>" alt="<?php echo esc_attr($partner['name']); ?>" loading="lazy">
+                        <a href="<?php echo esc_url($img_url); ?>" data-lightbox="partners-gallery" data-title="<?php echo esc_attr($partner['name']); ?>" class="orbit-partner-link">
+                            <div class="orbit-partner-node-inner">
+                                <span class="node-number"><?php echo sprintf('%02d', $partner['num']); ?></span>
+                                <div class="node-logo-wrapper">
+                                    <img src="<?php echo esc_url($img_url); ?>" alt="<?php echo esc_attr($partner['name']); ?>" loading="lazy">
+                                </div>
+                                <div class="node-tooltip">
+                                    <span class="tooltip-num">#<?php echo $partner['num']; ?></span>
+                                    <span class="tooltip-name"><?php echo esc_html($partner['name']); ?></span>
+                                </div>
                             </div>
-                            <div class="node-tooltip">
-                                <span class="tooltip-num">#<?php echo $partner['num']; ?></span>
-                                <span class="tooltip-name"><?php echo esc_html($partner['name']); ?></span>
-                            </div>
-                        </div>
+                        </a>
                     </div>
                 <?php endforeach; ?>
             </div>
