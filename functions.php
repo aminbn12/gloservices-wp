d288256c8cb95f8a51b7f2923b70f8d27f5b6777
Amine <benaminmedfr@gmail.com>
2026-08-06 17:29:56 +0100

    fix: resolve static page template pagination 404 error on projects page

--- functions.php
+++ functions.php
@@ -1783,3 +1783,67 @@ function gloservices_get_translated_page_url($slug) {
     return home_url('/' . $slug . '/');
 }
 
+/**
+ * Empêche l'erreur 404 sur la pagination de la page projets (page-projet.php)
+ */
+function gloservices_fix_page_projet_pagination($query) {
+    if ( ! is_admin() && $query->is_main_query() && ($query->is_page() || $query->is_singular('page')) ) {
+        $page_id = 0;
+        if ( isset($query->queried_object_id) && $query->queried_object_id ) {
+            $page_id = $query->queried_object_id;
+        } elseif ( isset($query->query_vars['page_id']) && $query->query_vars['page_id'] ) {
+            $page_id = $query->query_vars['page_id'];
+        } elseif ( isset($query->query_vars['pagename']) && $query->query_vars['pagename'] ) {
+            $page = get_page_by_path($query->query_vars['pagename']);
+            if ($page) {
+                $page_id = $page->ID;
+            }
+        }
+        
+        // Support Polylang (nettoyage du préfixe de langue dans le slug)
+        if ( ! $page_id && function_exists('pll_current_language') ) {
+            $pagename = isset($query->query_vars['pagename']) ? $query->query_vars['pagename'] : '';
+            if ( $pagename ) {
+                $parts = explode('/', $pagename);
+                $last_part = end($parts);
+                $page = get_page_by_path($last_part);
+                if ($page) {
+                    $page_id = $page->ID;
+                }
+            }
+        }
+
+        if ( $page_id ) {
+            $template = get_post_meta($page_id, '_wp_page_template', true);
+            if ( $template === 'page-projet.php' ) {
+                $paged = 1;
+                if ( get_query_var('page') ) {
+                    $paged = get_query_var('page');
+                } elseif ( isset($query->query_vars['page']) ) {
+                    $paged = $query->query_vars['page'];
+                }
+                
+                if ( $paged > 1 ) {
+                    $query->set('page', 0); // Empêche l'erreur 404
+                    $query->set('paged', $paged); // Définit la page pour la requête de projets
+                }
+            }
+        }
+    }
+}
+add_action('pre_get_posts', 'gloservices_fix_page_projet_pagination', 1);
+
+/**
+ * Désactive la redirection canonique automatique pour la pagination de la page projets
+ */
+function gloservices_disable_canonical_redirect_for_projet_pagination($redirect_url, $requested_url) {
+    if ( is_page() ) {
+        $template = get_post_meta(get_the_ID(), '_wp_page_template', true);
+        if ( $template === 'page-projet.php' ) {
+            return $requested_url; // Pas de redirection canonique
+        }
+    }
+    return $redirect_url;
+}
+add_filter('redirect_canonical', 'gloservices_disable_canonical_redirect_for_projet_pagination', 10, 2);
+
